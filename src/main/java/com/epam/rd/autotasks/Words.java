b577b76f464abdc57601989ac5916a15e5ee1f0b
Eugeniusz <eugenioskye@gmail.com>
2022-06-25 02:49:18 +0200

    second commit, works fine, regex problem solved

--- src/main/java/com/epam/rd/autotasks/Words.java
+++ src/main/java/com/epam/rd/autotasks/Words.java
@@ -15,6 +15,7 @@ public class Words {
         return finalValues.stream()
                 .map(s-> Entry.constructEntry(s, Words.countFrequency(flatPresortedMapped, s)))
                 .sorted(entryComparatorCountDesc.thenComparing(Entry::getEntry))
+                .filter(entry -> entry.count>9)
                 .map(Object::toString)
                 .collect(Collectors.joining("\n"));
     }
@@ -23,24 +24,17 @@ public class Words {
         return words.stream()
                 .map(s-> s.split(" "))
                 .flatMap(Arrays::stream)
-                .map(s -> s.replaceAll("\\p{Punct}", ""))
+                .map(s -> s.replaceAll("\\p{IsPunctuation}", " "))  //"\\p{Punct}"   [^A-Za-z]"  "\\p{IsPunctuation}
+                .map(s -> s.replaceAll("[ ]{2,}"," "))
+                .map(s -> s.split(" "))
+                .flatMap(Arrays::stream)
                 .filter(s -> s.length()>3)
                 .map(String::toLowerCase)
                 .collect(Collectors.toList());
     }
 
-//    firing was, he began to look for the general and his staff where they
-//    The command of the left flank belonged by seniority to the commander of
-//    and colonel looked sternly and significantly at one another like two
-//    the time had come to experience the joy of an attack of which he had so
-//    word infected the whole crowd with a feeling of panic.
-//
-//    But the staff officer did not finish what he wanted to say. A cannon
-//    Rostóv looked at and listened listlessly to what passed before and
-
     Comparator<Entry> entryComparatorCountAsc = Comparator.comparing(Entry::getCount);
     Comparator<Entry> entryComparatorCountDesc = entryComparatorCountAsc.reversed();
-
 
     private static int countFrequency(List<String> data, String entry){
         return (int) data.stream().filter(s -> s.equals(entry)).count();
@@ -74,93 +68,3 @@ public class Words {
         }
     }
 }
-
-
-
-//    public String countWords (List<String> lines){
-//
-//        List<String> preprocessed = lines.stream()
-//                .map( s -> s.replaceAll("\\p{Punct}", " "))
-//                .map(String::trim)
-//                .map( s -> s.split(" "))
-//                .flatMap(Arrays::stream)
-//                .filter( s -> s.length()<4)
-//                .map(String::toLowerCase)
-//                .collect(Collectors.toList());
-//
-//        List<String> unique = preprocessed.stream().distinct().collect(Collectors.toList());
-//
-//        return unique.stream()
-//                .map( s-> Entry.constructEntry( s, Words.countFrequency(preprocessed, s)))
-//                .filter( s -> s.count > 9)
-//                .sorted(entryComparatorCountDesc.thenComparing(Entry::getEntry))
-//                .map(Object::toString)
-//                .collect(Collectors.joining("\n"));
-//
-//    }
-
-
-//    private List<String> preprocess(List<String> words){
-//        return words.stream()
-//                .map(s-> s.split(" "))
-//                .flatMap(Arrays::stream)
-//                .map(s -> s.replaceAll("\\p{Punct}", ""))
-//                .filter(s -> s.length()>3)
-//                .map(String::toLowerCase)
-//                .collect(Collectors.toList());
-//    }
-
-//    private List<String> preprocess(List<String> words){
-//        return words.stream()
-//                .map(s-> s.split(" "))
-//                .flatMap(Arrays::stream)
-//                .map(s -> s.replaceAll("\\p{Punct}", " "))
-//                .map(s-> s.split(" "))
-//                .flatMap(Arrays::stream)
-//                .filter(s -> s.length()>3)
-//                .map(String::toLowerCase)
-//                .collect(Collectors.toList());
-//    }
-
-//                .split(" ").
-//        return line.map(String::trim)
-//                .map(String::toLowerCase)
-//                .map(s -> s.replaceAll("\\p{Punct}", " "))
-//                .map(s-> s.replaceAll("[ ]{2,}"," "))
-//                .map(s-> s.split(" "))
-//                .flatMap(Arrays::stream)
-//                .filter(s-> s.length()>3);
-
-//    public String countWords(List<String> lines) {
-//        List<String> preprocessed = lines.stream().map(Words::lineProcessor).flatMap(List::stream).collect(Collectors.toList());
-//        List<String> uniqueValues = preprocessed.stream().distinct().collect(Collectors.toList());
-//
-//        return uniqueValues.stream()
-//                .map( s-> Entry.constructEntry( s, Words.countFrequency(preprocessed, s)))
-//                .filter( s -> s.count > 9)
-//                .sorted(entryComparatorCountDesc.thenComparing(Entry::getEntry))
-//                .map(Object::toString)
-//                .collect(Collectors.joining("\n"));
-//    }
-//
-//    private static List<String> lineProcessor(String line){
-//        String temp = line.trim()
-//                .toLowerCase(Locale.ROOT)
-//                .replaceAll("\\p{Punct}", " ");
-//        line = line.replaceAll("[ ]{2,}"," ");
-//        return Arrays.stream(line.split(" ")).filter(s-> s.length()>3).collect(Collectors.toList());
-//
-//    }
-
-
-//    private List<String> preprocess(List<String> words){
-//        return words.stream()
-//                .map(s-> s.split(" "))
-//                .flatMap(Arrays::stream)
-//                .map(s -> s.replaceAll("\\p{Punct}", " "))
-//                .map(s-> s.split(" "))
-//                .flatMap(Arrays::stream)
-//                .filter(s -> s.length()>3)
-//                .map(String::toLowerCase)
-//                .collect(Collectors.toList());
-//    }
